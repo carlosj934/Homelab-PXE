a94e49915999d530a51921dc3be52aed7659633b
carlos <carlosjcorona123@gmail.com>
2024-07-14 13:08:25 -0700

    trying this

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,12 +1,12 @@
-# terraform {
-#   backend "http" {
-#     address = var.backend_address
-#     lock_address = var.lock_address
-#     unlock_address = var.unlock_address
-#     username = var.gitlab_user
-#     password = var.gitlab_token
-#     lock_method = "POST"
-#     unlock_method = "DELETE"
-#     retry_wait_min= "5"
-#   }
-# }
+terraform {
+  backend "http" {
+    # address = var.backend_address
+    # lock_address = var.lock_address
+    # unlock_address = var.unlock_address
+    # username = var.gitlab_user
+    # password = var.gitlab_token
+    # lock_method = "POST"
+    # unlock_method = "DELETE"
+    # retry_wait_min= "5"
+  }
+}
